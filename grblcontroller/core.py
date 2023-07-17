5e630220d1b61a6f1669b7c329708c136416de6d
fl028 <muffins.ziffer_0o@icloud.com>
2023-07-17 23:37:03 +0200

    ADD: move and tab functions

--- grblcontroller/core.py
+++ grblcontroller/core.py
@@ -8,6 +8,8 @@ class Controller:
         self.arduino = serial.Serial(port=port, baudrate=const.DEFAULT_BAUD)
         self._initialize()
         self._home()
+        self._demo_move()
+        self._demo_tab()
 
     def __del__(self):
         self.arduino.close()
@@ -16,25 +18,29 @@ class Controller:
     def read(self):
         output_lines = []
         while True:
-            if self.arduino.in_waiting > 0:
-                output_lines.append(self.arduino.read(self.arduino.in_waiting).decode(const.DEFAULT_ENCODING))
-                time.sleep(const.DEFAULT_SLEEP)
-            else:
+            while self.arduino.in_waiting == 0:
+                time.sleep(0.02)
+            output_lines.append(self.arduino.read(self.arduino.in_waiting).decode(const.DEFAULT_ENCODING))
+            if self.arduino.in_waiting == 0:
                 break
+        time.sleep(const.DEFAULT_SLEEP)
         print("Read: " + str(output_lines))
         return output_lines
 
-    def send(self, command):
+    def send_and_receive(self, command, check=True):
         self._write(command)
-        return self.read()  # Wait for read output after sending the command
+        output = self.read()
+
+        if check:
+            if "ok" not in output[0]:
+                raise Exception("Not ok")
 
     def _initialize(self):
         print("Init")
         self.arduino.close()
         self.arduino.open()
         self._get_port_state()
-        self._write(const.NEW_LINE_CHARACTER)
-        self.read()
+        self.send_and_receive(const.GRBL_WAKEUP_COMMAND,False)
 
     def _get_port_state(self):
         self.state = self.arduino.is_open
@@ -43,12 +49,25 @@ class Controller:
         print("Port state: " + str(self.state))
 
     def _write(self, input):
-        self.arduino.write(bytes(input + const.NEW_LINE_CHARACTER, const.DEFAULT_ENCODING))
-        time.sleep(const.DEFAULT_SLEEP)
+        print("Write: " + str(input).strip())
+        self.arduino.write(bytes(input, const.DEFAULT_ENCODING))
         self.arduino.flushInput()
         time.sleep(const.DEFAULT_SLEEP)
+        
 
     def _home(self):
         print("Home")
-        self.send(const.GRBL_HOME_COMMAND)
-        self.send(const.GRBL_ZERO_COMMAND)
+        self.send_and_receive(const.GRBL_HOME_COMMAND + const.NEW_LINE_CHARACTER)
+        self.send_and_receive(const.GRBL_ZERO_COMMAND + const.NEW_LINE_CHARACTER)
+
+    def _demo_move(self):
+        print("Move")
+        self.send_and_receive(const.GRBL_MOVE_COMMAND_2 + const.NEW_LINE_CHARACTER)
+        self.send_and_receive(const.GRBL_MOVE_COMMAND_1 + const.NEW_LINE_CHARACTER)
+
+    def _demo_tab(self):
+        print("Tab")
+        self.send_and_receive(const.GRBL_TAB_COMMAND_1 + const.NEW_LINE_CHARACTER)
+        self.send_and_receive(const.GRBL_TAB_COMMAND_2 + const.NEW_LINE_CHARACTER)
+        
+        
